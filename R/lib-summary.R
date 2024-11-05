480248c065a58c94da69e8f33d432e2d6055ce4b
kateaurora <katmcgivney@gmail.com>
2024-11-05 12:03:31 -0800

    added two functions within the acutal function of lib-summary

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,53 +1,79 @@
-#' Provide Number of R Packages by Library
+lib_summary <- function(sizes = FALSE) {
 
-#'
+  if (!is.logical(sizes)) {
 
-#' Provide the number of R package by library in
-
-#'  a data.frame
-
-#'
-
-#' @return a data.frame of R packages by library
-
-#' @export
-
-#'
-
-#' @examples
-
-#' lib_summary()
-#'
-#' @param sizes Should sizes of libraries be calculated.default is 'FALSE'.
-#'
-#'
-lib_summary <- function(sizes=FALSE) {
-if(!is.logical(sizes)){
     stop("'sizes' must be logical (TRUE or FALSE)")
 
-}
+  }
 
-  pkgs <- utils::installed.packages()
-  #tables the # of rows in lib path
-  pkg_tbl <- table(pkgs[, "LibPath"])
-  #turn into a df
+
+
+  pkg_df <- lib()
+
+  pkg_tbl <- table(pkg_df[, "LibPath"])
+
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
-  #give it some names
+
+
+
   names(pkg_df) <- c("Library", "n_packages")
-  #return the product at end of code execution
+
 
 
   if (isTRUE(sizes)) {
 
-    pkg_df$lib_size <- map_dbl(
-
-      pkg_df$Library,
-#new short hand for function (x)
-      \(x) sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
-
-    )
+    pkg_df <- calculate_sizes(pkg_df)
 
   }
+
+
+
   pkg_df
+
 }
 
+
+
+#' Generate a data frame of installed packages
+
+#'
+
+#' @return a data.frame of all packages installed on your system
+
+#' @export
+
+lib <- function() {
+
+  pkgs <- utils::installed.packages()
+
+  as.data.frame(pkgs, stringsAsFactors = FALSE)
+
+}
+
+
+
+#' calculate sizes
+
+#'
+
+#' @param df a data.frame
+
+#'
+
+#' @return df with a lib_size column
+
+#' @noRd
+
+calculate_sizes <- function(df) {
+
+  df$lib_size <- map_dbl(
+
+    df$Library,
+
+    \(x) sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+
+  )
+
+  df
+
+}
